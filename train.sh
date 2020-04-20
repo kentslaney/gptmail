7bb2dfc261d20b6b12f774c91c52a21e5a3f2333
Kent Slaney <kent@slaney.org>
2020-04-19 22:20:03 -0700

    I should probably start testing before committing

--- train.sh
+++ train.sh
@@ -9,13 +9,15 @@ cat data/clean/* | tr -d '\r' > data/all
 mkdir -p weights
 
 # train gpt2
+epochs="${1-10}"; shift
+out="${1-weights/default}"; shift
 python transformers/examples/run_language_modeling.py \
-	--output_dir=${2-weights/default} \
+	--output_dir="$out" \
 	--overwrite_output_dir \
 	--model_type=gpt2 \
 	--model_name_or_path=gpt2 \
 	--do_train \
 	--train_data_file=data/all \
 	--save_steps=5000 \
-	--num_train_epochs="${1-10}" \
+	--num_train_epochs="$epochs" \
 	"$@"
